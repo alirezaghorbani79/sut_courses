77fb7ce62f6e330d070135c8d060b797191b4689
reza adinepour <61472565+rezaAdinepour@users.noreply.github.com>
2022-07-14 14:02:12 +0430

    Update Circle.h

--- advanced_programming/HW01/solution_03/Q2/Circle.h
+++ advanced_programming/HW01/solution_03/Q2/Circle.h
@@ -1,1 +1,32 @@
+#pragma once
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Circle 
+{
+public:
+
+	Circle(float x, float y, float r, string n);
+	void setname(string s1);
+	void setCordinates(float x, float y);
+	void setRadius(float r); //with exception handling
+	void getinfo();
+	float getx_c();
+	float gety_c();
+	float getr();
+	string getname();
+	float perimeter();
+	float area();
+	void shiftCordinates(float x, float y);
+	void print();
+
+private:
+
+	float x_c;
+	float y_c;
+	float r;
+	string name;
+
+};
